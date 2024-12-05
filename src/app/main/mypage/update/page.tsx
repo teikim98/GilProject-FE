25589c64c2e71368cf16d25ee522dc320c7f98cb
teikim98 <teikim98@gmail.com>
2024-12-05 15:16:44 +0900

    업데이트 페이지 수정

--- src/app/main/mypage/update/page.tsx
+++ src/app/main/mypage/update/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { MouseEvent, ReactElement, useEffect, useState } from "react";
 import { Camera, Loader2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
@@ -42,6 +42,46 @@ export default function Page() {
     fetchProfile();
   }, []);
 
+
+  const updatePassword = () => {
+    setIsPasswordPopupOpen(true);
+  }
+
+
+  const handleProfileImgPopup = () => {
+    console.log("프로필 바꾸러 가쟈!!");
+    setIsUpdateImgPopupOpen(true);
+  }
+
+  const handleProfileImgVerified = (inputImgUrl: string) => {
+    if (inputImgUrl !== "") {
+      console.log(`입력된 프로필 이미지 Url: ${inputImgUrl}`);
+      setProfileInfo(prev => prev ? { ...prev, imageUrl: inputImgUrl } : prev);
+    }
+  }
+
+  const handleCommentPopup = (e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>) => {
+    //console.log("자기소개글 바꾸러 가쟈!!");
+    e.preventDefault();
+    setIsUpdateCommentPopupOpen(true);
+  }
+
+  const handleCommentVerified = (inputComment: string) => {
+    if (inputComment !== "") {
+      //console.log(`입력된 자기소개글로 바꾸기 : ${inputComment}`);
+      setProfileInfo(prev => prev ? { ...prev, comment: inputComment } : prev);
+    }
+  }
+
+  /**
+     * 데에터 변경 후 콜백 함수
+     * @param nickName
+     */
+  const dataChangeComplete = async (data: string) => {
+    console.log("변경된 data = " + data);
+    setProfileInfo(await getDetailProfile());
+  }
+
   if (error) {
     return (
       <div className='animate-fade-in flex flex-col pb-20'>
@@ -67,172 +107,135 @@ export default function Page() {
     );
   }
 
-  const updatePassword = () => {
-    setIsPasswordPopupOpen(true);
-  }
 
 
-  const handleProfileImgPopup = (e: React.MouseEvent) => {
-    console.log("프로필 바꾸러 가쟈!!");
-    e.preventDefault();
-    setIsUpdateImgPopupOpen(true);
-  }
-
-  const handleProfileImgVerified = (inputImgUrl: string) => {
-    if (inputImgUrl !== "") {
-      console.log(`입력된 프로필 이미지 Url: ${inputImgUrl}`);
-      setProfileInfo(prev => prev ? { ...prev, imageUrl: inputImgUrl } : prev);
-    }
-  }
-
-  const handleCommentPopup = (e: any) => {
-    //console.log("자기소개글 바꾸러 가쟈!!");
-    e.preventDefault();
-    setIsUpdateCommentPopupOpen(true);
-  }
-
-  const handleCommentVerified = (inputComment: string) => {
-    if (inputComment !== "") {
-      //console.log(`입력된 자기소개글로 바꾸기 : ${inputComment}`);
-      setProfileInfo(prev => prev ? { ...prev, comment: inputComment } : prev);
-    }
-  }
-
-  /**
-   * 데에터 변경 후 콜백 함수
-   * @param nickName
-   */
-  const dataChangeComplete = async (data: string) => {
-    console.log("변경된 data = " + data);
-    setProfileInfo(await getDetailProfile());
-  }
-
-  if (error) {
-    return (
-      <div className='animate-fade-in flex flex-col pb-20'>
-        <BackHeader content='마이 페이지' />
-        <Card className="w-full border-0 shadow-none">
-          <CardContent>
-            <div className="flex flex-col">
-              <Separator className='my-4 border-t-2 border-muted-foreground' />
-              <div className="flex flex-row items-center gap-5">
-                <span className="font-bold text-base">프로필</span>
-                <div className="relative">
-                  {profileInfo?.imageUrl ? (
-                    <img
-                      src={profileInfo.imageUrl}
-                      alt="Profile"
-                      className="w-12 h-12 rounded-full object-cover"
-                      onClick={(e) => {
-                        handleProfileImgPopup(e);
-                      }}
-                    />
-                  ) : (
-                    <Camera className="w-12 h-12 p-2 bg-muted rounded-full" />
-                  )}
-                </div>
-                <UpdateprofileImg imageUrl={profileInfo!.imageUrl} isPopupOpen={isUpdateImgPopupOpen} setIsPopupOpen={setIsUpdateImgPopupOpen} callback={handleProfileImgVerified} />
+  return (
+    <div className='animate-fade-in flex flex-col pb-20'>
+      <BackHeader content='마이 페이지' />
+      <Card className="w-full border-0 shadow-none">
+        <CardContent>
+          <div className="flex flex-col">
+            <Separator className='my-4 border-t-2 border-muted-foreground' />
+            <div className="flex flex-row items-center gap-5">
+              <span className="font-bold text-base">프로필</span>
+              <div className="relative" onClick={() => {
+                handleProfileImgPopup()
+              }}>
+                {profileInfo?.imageUrl ? (
+                  <img
+                    src={profileInfo.imageUrl}
+                    alt="Profile"
+                    className="w-12 h-12 rounded-full object-cover"
+                  />
+                ) : (
+                  <Camera className="w-12 h-12 p-2 bg-muted rounded-full" />
+                )}
               </div>
-              <Separator className='my-4 border-t-2 border-muted-foreground' />
-              <div className="flex flex-row items-center gap-5">
-                <span className="font-bold text-base">이름</span>
-                <Input className='w-[50%] text-sm bg-gray-100 border-muted'
-                  name="name"
-                  value={profileInfo?.name}
-                  readOnly
-                />
-              </div>
-              <Separator className='my-4 border-t-2 border-muted-foreground' />
-              <div className="flex flex-row items-center gap-5">
-                <span className="font-bold text-base">닉네임</span>
+              <UpdateprofileImg imageUrl={profileInfo!.imageUrl} isPopupOpen={isUpdateImgPopupOpen} setIsPopupOpen={setIsUpdateImgPopupOpen} callback={handleProfileImgVerified} />
+            </div>
+            <Separator className='my-4 border-t-2 border-muted-foreground' />
+            <div className="flex flex-row items-center gap-5">
+              <span className="font-bold text-base">이름</span>
+              <Input className='w-[50%] text-sm bg-gray-100 border-muted'
+                name="name"
+                value={profileInfo?.name}
+                readOnly
+              />
+            </div>
+            <Separator className='my-4 border-t-2 border-muted-foreground' />
+            <div className="flex flex-row justify-between items-center gap-5">
+              <div className="flex items-center gap-2"><span className="font-bold text-base">닉네임</span>
                 <Input className='w-[50%] text-sm'
                   name="nickName"
                   value={profileInfo?.nickName}
                   onChange={(e) => { setProfileInfo(prev => prev ? { ...prev, nickName: e.target.value } : prev); }}
-                />
-                <Button
-                  onClick={() => { setIsNickNamePopupOpen(true) }}
-                  className="w-[10%]"
-                >변경</Button>
-              </div>
-              <Separator className='my-4 border-t-2 border-muted-foreground' />
-              <div className="flex flex-row items-center gap-5">
-                <span className="font-bold text-base">이메일</span>
-                <Input className='w-[50%] text-sm bg-gray-100 border-muted'
-                  name="email"
-                  value={profileInfo?.email || '이메일 미설정'}
-                  readOnly
-                />
-              </div>
-              <Separator className='my-4 border-t-2 border-muted-foreground' />
-              <div className="flex flex-row items-center gap-5">
-                <span className="font-bold text-base">주소</span>
-                <Input className='w-[50%] text-sm'
-                  name="address"
-                  value={profileInfo?.address || '주소 미설정'}
-                  readOnly
-                />
-                <Button
-                  onClick={() => { setIsAddressPopupOpen(true) }}
-                  className="w-[10%]"
-                >변경</Button>
-              </div>
-              <Separator className='my-4 border-t-2 border-muted-foreground' />
-              <div className="flex flex-row items-center gap-5">
-                <span className="font-bold text-base">자기소개</span>
-                <Input className='w-[50%] text-sm'
-                  name="comment"
-                  value={profileInfo?.comment || ""}
-                  readOnly
-                  onChange={(e) => setProfileInfo(prev => prev ? { ...prev, comment: e.target.value } : null)}
-                  placeholder='자기소개가 없습니다'
-                />
-                <Button
-                  onClick={(e) => {
-                    handleCommentPopup(e);
-                  }}
-                  className="w-[10%]"
-                >변경</Button>
-                <UpdateComment postComment={profileInfo!.comment} isPopupOpen={isUpdateCommentPopupOpen} setIsPopupOpen={setIsUpdateCommentPopupOpen} callback={handleCommentVerified} />
-              </div>
-              <Separator className='my-4 border-t-2 border-muted-foreground' />
-              <div className="flex flex-row items-center gap-5">
-                <span className="font-bold text-base">가입일자</span>
-                <Input className='w-[50%] text-sm bg-gray-100 border-muted'
-                  name="comment"
-                  value={profileInfo?.joinDate.split("T")[0]}
-                  readOnly
-                />
-              </div>
-              <Separator className='my-4 border-t-2 border-muted-foreground' />
-              <div className="flex flex-row items-center gap-5">
-                <span className="font-bold text-base">비밀번호</span>
-                <Button
-                  onClick={updatePassword}
-                  className="w-[30%] bg-red-600 text-white hover:bg-red-700"
-                >
-                  비밀번호 변경
-                </Button>
-              </div>
-              <Separator className='my-4 border-t-2 border-muted-foreground' />
-              <div className="flex flex-col items-center justify-center space-y-4">
+                /></div>
 
-              </div>
+              <Button
+                onClick={() => { setIsNickNamePopupOpen(true) }}
+                className="px-4"
+              >변경</Button>
             </div>
-          </CardContent>
-          <CardFooter className="px-6 pt-0 flex justify-center">
+            <Separator className='my-4 border-t-2 border-muted-foreground' />
+            <div className="flex flex-row items-center gap-5">
+              <span className="font-bold text-base">이메일</span>
+              <Input className='w-[50%] text-sm bg-gray-100 border-muted'
+                name="email"
+                value={profileInfo?.email || '이메일 미설정'}
+                readOnly
+              />
+            </div>
+            <Separator className='my-4 border-t-2 border-muted-foreground' />
+            <div className="flex flex-row items-center gap-5">
+              <span className="font-bold text-base">주소</span>
+              <Input className='w-[50%] text-sm'
+                name="address"
+                value={profileInfo?.address || '주소 미설정'}
+                readOnly
+              />
+              <Button
+                onClick={() => { setIsAddressPopupOpen(true) }}
+                className="w-[10%]"
+              >변경</Button>
+            </div>
+            <Separator className='my-4 border-t-2 border-muted-foreground' />
+            <div className="flex flex-row items-center gap-5">
+              <span className="font-bold text-base">자기소개</span>
+              <Input className='w-[50%] text-sm'
+                name="comment"
+                value={profileInfo?.comment || ""}
+                readOnly
+                onChange={(e) => setProfileInfo(prev => prev ? { ...prev, comment: e.target.value } : null)}
+                placeholder='자기소개가 없습니다'
+              />
+              <Button
+                onClick={(e) => {
+                  handleCommentPopup(e);
+                }}
+                className="w-[10%]"
+              >변경</Button>
+              <UpdateComment postComment={profileInfo!.comment} isPopupOpen={isUpdateCommentPopupOpen} setIsPopupOpen={setIsUpdateCommentPopupOpen} callback={handleCommentVerified} />
+            </div>
+            <Separator className='my-4 border-t-2 border-muted-foreground' />
+            <div className="flex flex-row items-center gap-5">
+              <span className="font-bold text-base">가입일자</span>
+              <Input className='w-[50%] text-sm bg-gray-100 border-muted'
+                name="comment"
+                value={profileInfo?.joinDate.split("T")[0]}
+                readOnly
+              />
+            </div>
+            <Separator className='my-4 border-t-2 border-muted-foreground' />
+            <div className="flex flex-row items-center gap-5">
+              <span className="font-bold text-base">비밀번호</span>
+              <Button
+                onClick={updatePassword}
+                className="w-[30%] bg-red-600 text-white hover:bg-red-700"
+              >
+                비밀번호 변경
+              </Button>
+            </div>
+            <Separator className='my-4 border-t-2 border-muted-foreground' />
+            <div className="flex flex-col items-center justify-center space-y-4">
 
-          </CardFooter>
-        </Card>
-        <NickNameChangePopup initialData={profileInfo?.nickName} isPopupOpen={isNickNamePopupOpen} setIsPopupOpen={setIsNickNamePopupOpen} callback={dataChangeComplete} />
-        <AddressChangePopup props={{
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className="px-6 pt-0 flex justify-center">
+
+        </CardFooter>
+      </Card>
+      <NickNameChangePopup initialData={profileInfo?.nickName} isPopupOpen={isNickNamePopupOpen} setIsPopupOpen={setIsNickNamePopupOpen} callback={dataChangeComplete} />
+      <AddressChangePopup
+        props={{
           isPopupOpen: isAddressPopupOpen,
           setIsPopupOpen: setIsAddressPopupOpen,
-          callback: dataChangeComplete
-        }} isMypage={true} />
-        <PasswordChangePopup isPopupOpen={isPasswordPopupOpen} setIsPopupOpen={setIsPasswordPopupOpen} callback={() => { }} />
-      </div>
-    );
-  }
+          callback: dataChangeComplete,
+        }}
+        isMypage={true}
+      />
+      <PasswordChangePopup isPopupOpen={isPasswordPopupOpen} setIsPopupOpen={setIsPasswordPopupOpen} callback={() => { }} />
+    </div>
 
+  );
 }
