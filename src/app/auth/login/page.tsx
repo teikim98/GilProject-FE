f633dfc3d94726efba838a3bfa1cf7beaf85f391
JHW <jeedd95@gmail.com>
2024-12-07 10:15:03 +0900

    주소 변경

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -107,7 +107,7 @@ const HomePage = () => {
                   {/* Social Login Buttons */}
                   <div className="flex flex-row justify-center items-center gap-4 w-full">
                     <div className="flex-1 flex justify-center">
-                    <Link href={`${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/google`} passHref>
+                    <Link href={`${process.env.NEXT_PUBLIC_API_URL}/login/oauth2/code/google`} passHref>
                         <Avatar className="cursor-pointer">
                           <AvatarImage src="/Resources/Google/web_neutral_sq_na@4x.png" />
                         </Avatar>
@@ -115,7 +115,8 @@ const HomePage = () => {
                     </div>
 
                     <div className="flex-1 flex justify-center">
-                      <Link href={`${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/naver`} passHref>
+                      {/* <Link href={`${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/naver`} passHref> */}
+                      <Link href={`${process.env.NEXT_PUBLIC_API_URL}/login/oauth2/code/naver`} passHref>
                         <Avatar className="cursor-pointer">
                           <AvatarImage src="/Resources/Naver/btnG_아이콘사각.png" />
                         </Avatar>
@@ -123,7 +124,7 @@ const HomePage = () => {
                     </div>
 
                     <div className="flex-1 flex justify-center">
-                      <Link href={`${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao`} passHref>
+                      <Link href={`${process.env.NEXT_PUBLIC_API_URL}/login/oauth2/code/kakao`} passHref>
                         <Avatar className="cursor-pointer">
                           <AvatarImage src="/Resources/Kakao/카카오톡.png" />
                         </Avatar>
